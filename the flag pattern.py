0ed451f6d825d6095332366ae022193ce5e3312c
Sum <ddonation45@gmail.com>
2026-09-07 22:30:02 +0530

    Add files via upload

---
+++ the flag pattern.py
@@ -0,0 +1,13 @@
+numbers = [12,23,34,56,78,90]
+
+found = False
+
+for num in numbers:
+    if num == 55:
+     found = True
+
+if found:
+   print("number found")
+else:
+   print("number not found")
+
