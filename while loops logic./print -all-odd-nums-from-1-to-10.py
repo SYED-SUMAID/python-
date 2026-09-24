16a8a006070c0764ac85baed08fcb95462c4b882
SYED SUMAID <ddonation45@gmail.com>
2026-09-24 12:26:09 +0530

    Add while loop practice

---
+++ while loops logic./print -all-odd-nums-from-1-to-10.py
@@ -0,0 +1,7 @@
+i = 1
+
+while i <=20:
+    if i % 2 == 1:
+     print(i)
+    i +=1
+     
--- while loops logic./print nums from1 to 10.py
+++ while loops logic./print nums from1 to 10.py
@@ -1,4 +1,7 @@
-i = 0 
-while i < 10:
-    i += 1
-    print(i)
+i = 1
+total = 0
+
+while i<= 10:
+    total = total+i 
+    i += 1 
+print(total)
